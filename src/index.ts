dc3c01a3c291cb86a114f07b05e115f3e79a9d28
Navneet <1611navneetkumar@gmail.com>
2025-02-04 15:13:44 +0530

    Making a todo web in type script

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,16 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+// import { v4 as uuidV4 } from "uuid";
 
-import confetti from 'canvas-confetti';
+// console.log(uuidV4())
 
-confetti.create(document.getElementById('canvas') as HTMLCanvasElement, {
-  resize: true,
-  useWorker: true,
-})({ particleCount: 200, spread: 200 });
+
+const list = document.querySelector<HTMLUListElement>("#list")
+const from = document.getElementById("#new-task-from")as HTMLFormElement |null
+const input = document.querySelector<HTMLInputElement>("#new-task-title")
+
+ from?.addEventListener("sumit",e => {
+  e.preventDefault()
+
+  if(input?.value == "" || input?.value ==null) return
+
+  input.value
+ })
